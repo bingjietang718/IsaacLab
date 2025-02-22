dddcb556f805682ead4c8593f4f31e4870f0392d
Bingjie Tang <btang@nvidia.com>
2025-02-21 19:50:15 -0800

    add a script to update assembly id through commandline

--- source/isaaclab_tasks/isaaclab_tasks/direct/assembly/assembly_tasks_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/assembly/assembly_tasks_cfg.py
@@ -121,10 +121,10 @@ class AssemblyTask:
     curriculum_height_step: list = [-0.005, 0.003]  # how much to increase max initial downward displacement after hitting success or failure thresh
     curriculum_height_bound: list = [-0.01, 0.01]  # max initial downward displacement of plug at hardest and easiest stages of curriculum
 
-    if_sbc: bool = False 
+    if_sbc: bool = True 
 
     # Logging evaluation results
-    if_logging_eval: bool = True
+    if_logging_eval: bool = False
     num_eval_trials: int = 32
     eval_filename: str = 'evaluation.h5'
 
@@ -148,9 +148,7 @@ class Hole8mm(FixedAssetCfg):
 class Insertion(AssemblyTask):
     name = 'insertion'
 
-    # assembly_id = '00731'
-    # assembly_id = '00015'
-    assembly_id = '00081'
+    assembly_id = '00015'
     assembly_dir = f'{ASSET_DIR}/{assembly_id}/'
 
     fixed_asset_cfg = Hole8mm()
