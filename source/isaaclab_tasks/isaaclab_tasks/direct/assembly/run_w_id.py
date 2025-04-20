a741d1cfa0e5f7e6485ec60e53ea8ff51e864f18
Bingjie Tang <btang@nvidia.com>
2025-04-19 18:41:33 -0700

    update run dir name during ft

--- source/isaaclab_tasks/isaaclab_tasks/direct/assembly/run_w_id.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/assembly/run_w_id.py
@@ -78,12 +78,6 @@ def main():
         run_name = f"automate_{date_str}_{args.assembly_id}"
         bash_command += f" agent.params.config.name={run_name}"
 
-        if args.sample == 'gp':
-            run_name += f"_ft_{args.ac_func}"
-            bash_command += f" agent.params.config.max_epochs=200"
-            
-        bash_command += f" agent.params.config.name={run_name}"
-
     # Run the bash command
     subprocess.run(bash_command, shell=True, check=True)
 
